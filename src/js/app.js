f030903b998725f2f6ecf3f0dbb3351031ed2c25
Pedro Rodriguez Alia <pedro.rodalia@gmail.com>
2018-10-02 20:59:33 +0200

    react - react skeleton in src dir

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component () {
+  render() {
+    return(
+      <div className="app">
+        <h1>Hello World!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
