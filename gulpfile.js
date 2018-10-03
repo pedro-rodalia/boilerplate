bf935fa352881e4650b162838db125ce4eafe321
Pedro Rodriguez Alia <pedro.rodalia@gmail.com>
2018-10-03 22:12:55 +0200

    gulp - gulpfile working w/ v4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'); 
+
+gulp.task('build', (done) => {
+  console.log('working');
+  done();
+});
+
+gulp.task('start', (done) => {
+  console.log('start');
+  done();
+});
+
+gulp.task('default', gulp.series('build', 'start', (done) => {
+  console.log('done');
+  done();
+}))
