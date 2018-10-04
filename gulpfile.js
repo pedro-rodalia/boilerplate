0daac631bda47d4cb8e4024df7d5fd4c1c461963
Pedro Rodriguez Alia <pedro.rodalia@gmail.com>
2018-10-04 18:12:11 +0200

    gulp - Webpack build and server start both with watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
-var gulp = require('gulp'); 
-
-gulp.task('build', (done) => {
-  console.log('working');
-  done();
-});
+var gulp = require('gulp');
+var webpack = require('webpack');
+var gulpwebpack = require('webpack-stream');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('start', (done) => {
-  console.log('start');
+  console.log('Starting server and watching with nodemon');
+  nodemon({
+     script: './bin/www',
+     ext: 'js html',
+     env: { 'NODE_ENV': 'development' }
+ });
   done();
 });
 
-gulp.task('default', gulp.series('build', 'start', (done) => {
-  console.log('done');
+gulp.task('build', (done) => {
+  console.log('Bundling with webpack and watching files');
+  gulp.src('src/index.js')
+    .pipe(gulpwebpack( require('./webpack.config.js') ), webpack)
+    .pipe(gulp.dest('public/dist'));
   done();
-}))
+});
+
+gulp.task('default', gulp.series(['build', 'start'], (done) => {
+  console.log('Starting server');
+  done();
+}));
