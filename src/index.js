ecc6f27afc2efba78190e3e2ed79a525ed2b6796
Pedro Rodriguez Alia <pedro.rodalia@gmail.com>
2018-10-15 23:12:45 +0200

    appearance - some styling and eslint config

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './js/app.js';
 import './css/styles.css';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab);
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const App = () => {
-  return (<div className="app">
-    <h1 className="font-sans text-lg text-grey-darkest text-center text-5xl font-hairline mt-32">Hello World!</h1>
-    <h2 className="font-sans text-lg text-grey-darkest text-center text-2xl font-hairline mt-3">I'm using tailwind!</h2>
-  </div>)
+  return (
+    <div className="app">
+      <div className="content">
+        <h1>MEAN Boilerplate</h1>
+        <h2><FontAwesomeIcon icon={['fab', 'node-js']}/> Node.js</h2>
+        <h2><FontAwesomeIcon icon={['fab', 'react']}/> React.js</h2>
+      </div>
+      <div className="links">
+        <button className="btn"><FontAwesomeIcon icon={['fab', 'github']}/> Github</button>
+        <button className="btn"><FontAwesomeIcon icon={['fab', 'bitbucket']}/> Bitbucket</button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
